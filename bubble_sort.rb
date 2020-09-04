191c1542dd5bc604178c115a4015478f6dc548be
Nicolas <nicodsm7@gmail.com>
2020-09-04 10:32:16 +0200

    Update bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,18 @@
 def bubble_sort(numbers)
     changes_made=false;
     tail=numbers.length-2;
-    (numbers.length).times do
+    loop do
+        changes_made=false;
         for i in 0..tail
             if(numbers[i]>numbers[i+1])
                 zw=numbers[i+1];
                 numbers[i+1]=numbers[i];
                 numbers[i]=zw;
                 changes_made=true;
-            else
-                changes_made=false;
             end
         end
-        tail-=1;
-    end      
+        break if (changes_made==false);
+    end
     return numbers;
 end
-print bubble_sort([4,3,78,2,0,2])
+print bubble_sort([4,3,78,5,-77,2])
